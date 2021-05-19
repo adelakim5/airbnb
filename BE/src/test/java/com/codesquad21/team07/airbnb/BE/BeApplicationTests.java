c42798959c0d823bd6dc9e41463b5790102cce96
sanhee <68936@naver.com>
2021-05-19 19:51:30 +0900

    feat: 스프링부트 초기설정

---
+++ BE/src/test/java/com/codesquad21/team07/airbnb/BE/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesquad21.team07.airbnb.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
