72695defdc486e095905ff17bb86aaa5e64882ea
adelakim5 <49264795+adelakim5@users.noreply.github.com>
2021-06-05 12:38:08 +0900

    Revert "chore: build 관련"

--- fe/src/components/accomodation/private.js
+++
@@ -1,4 +0,0 @@
-export const apiKey = 'AIzaSyAG6a29gO1LLdgcVI3nhqFVHPt_fZioj10';
-
-export const test =
-    'http://airbnb.clone.r-e.kr/api/rooms?locationId=19&checkIn=2021-06-04&checkOut=2021-06-08&priceMin=90000&priceMax=320000&adults=1&children=0&infants=0';
